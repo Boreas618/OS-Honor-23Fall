32559666f4d58211bdd7a8acb55a39d3fa533095
Boreas618 <21307130094@m.fudan.edu.cn>
2023-12-12 22:40:04 +0800

    style: code

--- src/kernel/mem.h
+++ src/kernel/mem.h
@@ -1,38 +1,38 @@
-#pragma once
-
-#include <common/defines.h>
 #include <aarch64/mmu.h>
 #include <common/defines.h>
 #include <common/list.h>
 #include <common/rc.h>
 
-#define PAGE_COUNT ((P2K(PHYSTOP) - PAGE_BASE((u64) & end)) / PAGE_SIZE - 1)
+#define MAX_BUCKETS 16
+#define MAX_PAGES 1048576
+#define PAGE_COUNT ((P2K(PHYSTOP) - PAGE_BASE((u64) &end)) / PAGE_SIZE - 1)
+#define VA2ID(vaddr) \
+    ((vaddr - PAGE_BASE((u64) &end) - PAGE_SIZE) / PAGE_SIZE)
 
-u64 left_page_cnt();
+typedef struct Page Page;
+typedef struct PartitionedPageNode PartitionedPageNode;
 
-WARN_RESULT void *get_zero_page();
-
-typedef struct partitioned_page_node PartitionedPageNode;
-
-typedef struct partitioned_page_node {
-    struct ListNode pp_node;
-    struct page* page;
+struct PartitionedPageNode {
+    ListNode pp_node;
+    Page *page;
     u8 bucket_index;
-} PartitionedPageNode;
+};
 
-typedef struct page {
+struct Page {
     u64 addr;
     u32 base_size;
     u64 free_head;
     u32 alloc_partitions_cnt;
     PartitionedPageNode partitioned_node;
     RefCount ref;
-} Page;
+};
 
-u16 _round_up(isize s, u32* rounded_size, u8* bucket_index);
-u64 _alloc_partition(Page* p);
-PartitionedPageNode* _partition_page(u32 rounded_size, u8 bucket_index);
-WARN_RESULT void* kalloc_page();
-void kfree_page(void* page);
-WARN_RESULT void* kalloc(isize size);
-void kfree(void* ptr);
+u64 left_page_cnt(void);
+WARN_RESULT void *get_zero_page(void);
+u16 __round_up(isize s, u32 *rounded_size, u8 *bucket_index);
+u64 __alloc_partition(Page *p);
+PartitionedPageNode *__partition_page(u32 rounded_size, u8 bucket_index);
+WARN_RESULT void *kalloc_page(void);
+void kfree_page(void *page);
+WARN_RESULT void *kalloc(isize size);
+void kfree(void *ptr);
