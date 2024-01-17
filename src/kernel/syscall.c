70d1ae29ddf1cb92d9d52dbbf0d82234fbc02950
Boreas618 <21307130094@m.fudan.edu.cn>
2024-01-18 05:55:36 +0800

    feat: user_readable and user_writable

--- src/kernel/syscall.c
+++ src/kernel/syscall.c
@@ -9,7 +9,6 @@ void *syscall_table[NR_SYSCALL];
 
 void syscall_entry(UserContext *context) {
     u64 id = context->gregs[8];
-
     u64 arg0 = context->gregs[0];
     u64 arg1 = context->gregs[1];
     u64 arg2 = context->gregs[2];
@@ -17,33 +16,43 @@ void syscall_entry(UserContext *context) {
     u64 arg4 = context->gregs[4];
     u64 arg5 = context->gregs[5];
 
-    (void)((id < NR_SYSCALL) && (syscall_table[id] != NULL) &&
-           (context->gregs[0] =
-                ((u64(*)(u64, u64, u64, u64, u64, u64))syscall_table[id])(
-                    arg0, arg1, arg2, arg3, arg4, arg5)));
+    if ((id < NR_SYSCALL) && (syscall_table[id] != NULL)) {
+        context->gregs[0] =
+            ((u64(*)(u64, u64, u64, u64, u64, u64))syscall_table[id])(
+                arg0, arg1, arg2, arg3, arg4, arg5);
+    }
 }
 
-// check if the virtual address [start,start+size) is READABLE by the current
-// user process
+/**
+ * Check if the virtual address [start,start+size) is READABLE by the current
+ * user process
+ */
 bool user_readable(const void *start, usize size) {
-    // TODO
-    (void)start;
-    (void)size;
-    return 0;
+    struct list vmregions = thisproc()->vmspace.vmregions;
+    list_forall(p, vmregions) {
+        struct vmregion *v = container_of(p, struct vmregion, stnode);
+        if (v->begin <= (u64)start && ((u64)start) + size <= v->begin)
+            return true;
+    }
+    return false;
 }
 
-// check if the virtual address [start,start+size) is READABLE & WRITEABLE by
-// the current user process
+/* Check if the virtual address [start,start+size) is READABLE & WRITEABLE by
+ * the current user process. */
 bool user_writeable(const void *start, usize size) {
-    // TODO
-    (void)start;
-    (void)size;
-    return 0;
+    struct list vmregions = thisproc()->vmspace.vmregions;
+    list_forall(p, vmregions) {
+        struct vmregion *v = container_of(p, struct vmregion, stnode);
+        if (!(v->flags & ST_RO) && v->begin <= (u64)start &&
+            ((u64)start) + size <= v->begin)
+            return true;
+    }
+    return false;
 }
 
-// get the length of a string including tailing '\0' in the memory space of
-// current user process return 0 if the length exceeds maxlen or the string is
-// not readable by the current user process
+/* Get the length of a string including tailing '\0' in the memory space of
+ * current user process return 0 if the length exceeds maxlen or the string is
+ * not readable by the current user process */
 usize user_strlen(const char *str, usize maxlen) {
     for (usize i = 0; i < maxlen; i++) {
         if (user_readable(&str[i], 1)) {
