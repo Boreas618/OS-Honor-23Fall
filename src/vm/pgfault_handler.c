5cdc93d7a616c8e386473b04d99ccab09b3d8481
Boreas618 <21307130094@m.fudan.edu.cn>
2024-01-25 05:33:19 +0800

    style

--- src/vm/pgfault_handler.c
+++ src/vm/pgfault_handler.c
@@ -4,7 +4,7 @@
 #include <proc/sched.h>
 #include <vm/vmregion.h>
 
-void handle_copy_on_write(pgtbl_entry_t *pte, struct vmspace* vs, u64 addr)
+void handle_copy_on_write(pgtbl_entry_t *pte, struct vmspace *vs, u64 addr)
 {
 	void *target_page = (void *)P2K(PTE_ADDRESS(*pte));
 	void *new_page = kalloc_page();
@@ -18,10 +18,7 @@ int pgfault_handler(u64 iss)
 	(void)iss;
 	struct proc *p = thisproc();
 	struct vmspace *vs = &p->vmspace;
-
-	/* The address which caused the page fault. */
-	u64 addr = arch_get_far();
-
+	u64 addr = arch_get_far(); // The address which caused the page fault.
 	pgtbl_entry_t *pte = get_pte(vs->pgtbl, addr, false);
 
 	list_forall(p, vs->vmregions)
@@ -31,14 +28,13 @@ int pgfault_handler(u64 iss)
 			bool vmr_read_only = (v->flags & VMR_RO);
 			bool page_read_only = (PTE_FLAGS(*pte) & PTE_RO);
 
-            // Copy on Write.
+			// Copy on Write.
 			if (!vmr_read_only && page_read_only) {
 				handle_copy_on_write(pte, vs, addr);
 				goto finished;
 			}
 		}
 	}
-	ASSERT(1 == 2);
 
 finished:
 	// We have modified the page table. Therefore, we should flush TLB to ensure
